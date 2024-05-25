0e4c44705a3cf30c12ede726c922a24b49a70a5d
Frieso Turkstra <friesoturkstra@gmail.com>
2024-05-25 18:05:10 +0200

    update

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -62,26 +62,31 @@ def main():
     load_data(directory)
     print("Data loaded.")
 
-    source = person_id_for_name(input("Name: "))
-    if source is None:
-        sys.exit("Person not found.")
-    target = person_id_for_name(input("Name: "))
-    if target is None:
-        sys.exit("Person not found.")
+    while True:
+        source = person_id_for_name(input("Name: "))
+        if source is None:
+            print("Person not found.")
+            continue
+            #sys.exit("Person not found.")
+        target = person_id_for_name(input("Name: "))
+        if target is None:
+            print("Person not found.")
+            continue
+            #sys.exit("Person not found.")
 
-    path = shortest_path(source, target)
+        path = shortest_path(source, target)
 
-    if path is None:
-        print("Not connected.")
-    else:
-        degrees = len(path)
-        print(f"{degrees} degrees of separation.")
-        path = [(None, source)] + path
-        for i in range(degrees):
-            person1 = people[path[i][1]]["name"]
-            person2 = people[path[i + 1][1]]["name"]
-            movie = movies[path[i + 1][0]]["title"]
-            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+        if path is None:
+            print("Not connected.")
+        else:
+            degrees = len(path)
+            print(f"{degrees} degrees of separation.")
+            path = [(None, source)] + path
+            for i in range(degrees):
+                person1 = people[path[i][1]]["name"]
+                person2 = people[path[i + 1][1]]["name"]
+                movie = movies[path[i + 1][0]]["title"]
+                print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
 
 def shortest_path(source, target):
